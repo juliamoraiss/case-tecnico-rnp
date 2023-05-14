b5f5b68bf493b3753b675020779d34f4987235f4
Júlia - A3 Data <julia-a3data@MacBook-Pro.local>
2023-05-13 23:14:35 -0300

    remove filemd5 for test

--- infrastructure/aws/s3_files.tf
+++ infrastructure/aws/s3_files.tf
@@ -3,5 +3,4 @@ resource "aws_s3_bucket_object" "delta_insert" {
   key    = "emr-code/pyspark/delta_spark_insert.py"
   acl    = "private"
   source = "../etl/delta_spark_insert.py"
-  etag   = filemd5("../etl/delta_spark_insert.py")
 }

f11bb286cd1460878746fd52dacdeade3f0dba77
Júlia - A3 Data <julia-a3data@MacBook-Pro.local>
2023-05-13 23:13:17 -0300

    fix bucket name

--- infrastructure/aws/s3_files.tf
+++ infrastructure/aws/s3_files.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_object" "delta_insert" {
-  bucket = aws_s3_bucket.datalake.id
+  bucket = "${var.bucket_name}"
   key    = "emr-code/pyspark/delta_spark_insert.py"
   acl    = "private"
   source = "../etl/delta_spark_insert.py"

167a9b4f9097a9bfddf18f1df03b8ab40e1aa345
Júlia - A3 Data <julia-a3data@MacBook-Pro.local>
2023-05-15 16:50:40 -0300

    add bootsrat file and change step just for test

--- config/bootstrap_emr.sh
+++ config/bootstrap_emr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo aws s3 cp s3://delta-lake-project-config/jars/ /usr/lib/spark/jars/ --recursive
+sudo aws s3 cp s3://rnp-datalake/config/jars/ /usr/lib/spark/jars/ --recursive
 sudo python3 -m pip install --upgrade pip setuptools wheel
 sudo pip3 install pandas==1.1.5 -v
 sudo pip3 install numpy==1.19.5 -v

7c30d733d64691b4bde43bd13b63ed7117dda1aa
Júlia - A3 Data <julia-a3data@MacBook-Pro.local>
2023-05-15 23:24:40 -0300

    fix bootstrap file

--- config/bootstrap_emr.sh
+++ config/bootstrap_emr.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo aws s3 cp s3://rnp-datalake/config/jars/ /usr/lib/spark/jars/ --recursive
-sudo python3 -m pip install --upgrade pip setuptools wheel
-sudo pip3 install pandas==1.1.5 -v
-sudo pip3 install numpy==1.19.5 -v
-sudo pip3 install boto3
-sudo pip3 install requests
-sudo pip3 install fuzzywuzzy==0.18.0
+
+sudo python3 -m pip install \
+    pandas==1.1.5 -v \
+    numpy==1.19.5 -v \
+    boto3 \
+    requests \
+    fuzzywuzzy==0.18.0

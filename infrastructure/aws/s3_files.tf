a8c53bf3a25ba5dac67223e4151e59391d75ee19
Júlia - A3 Data <julia-a3data@MacBook-Pro.local>
2023-05-16 10:07:27 -0300

    add upsert file to s3

--- infrastructure/aws/s3_files.tf
+++ infrastructure/aws/s3_files.tf
@@ -6,6 +6,14 @@ resource "aws_s3_bucket_object" "delta_insert" {
   etag = filemd5("../../etl/delta_spark_insert.py")
 }
 
+resource "aws_s3_bucket_object" "delta_insert" {
+  bucket = aws_s3_bucket.datalake.id
+  key    = "emr-code/pyspark/delta_spark_insert.py"
+  acl    = "private"
+  source = "../../etl/delta_spark_insert.py"
+  etag = filemd5("../../etl/delta_spark_upsert.py")
+}
+
 resource "aws_s3_bucket_object" "extract_doi" {
   bucket = aws_s3_bucket.datalake.id
   key    = "emr-code/pyspark/extract.py"
